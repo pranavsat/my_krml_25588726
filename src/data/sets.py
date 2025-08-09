da0f2c6d2698816ee78e2311ac887e8dff9aa517
pranavsat <pranavsathya56@gmail.com>
2025-08-09 21:02:43 +1000

    Add new sets.py, update dates.py and performance.py, bump version to 2025.0.1.1

---
+++ src/data/sets.py
@@ -0,0 +1,96 @@
+def subset_x_y(target, features, start_index:int, end_index:int):
+    """Keep only the rows for X and y (optional) sets from the specified indexes
+
+    Parameters
+    ----------
+    target : pd.DataFrame
+        Dataframe containing the target
+    features : pd.DataFrame
+        Dataframe containing all features
+    features : int
+        Index of the starting observation
+    features : int
+        Index of the ending observation
+
+    Returns
+    -------
+    pd.DataFrame
+        Subsetted Pandas dataframe containing the target
+    pd.DataFrame
+        Subsetted Pandas dataframe containing all features
+    """
+
+    return features[start_index:end_index], target[start_index:end_index]
+
+def split_sets_by_time(df, target_col, test_ratio=0.2):
+    """Split sets by indexes for an ordered dataframe
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe
+    target_col : str
+        Name of the target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+
+    df_copy = df.copy()
+    target = df_copy.pop(target_col)
+    cutoff = int(len(df_copy) / 5)
+
+    X_train, y_train = subset_x_y(target=target, features=df_copy, start_index=0, end_index=-cutoff*2)
+    X_val, y_val     = subset_x_y(target=target, features=df_copy, start_index=-cutoff*2, end_index=-cutoff)
+    X_test, y_test   = subset_x_y(target=target, features=df_copy, start_index=-cutoff, end_index=len(df_copy))
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def split_sets_random(features, target, test_ratio=0.2):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    features : pd.DataFrame
+        Input dataframe
+    target : pd.Series
+        Target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    from sklearn.model_selection import train_test_split
+
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
